3f0ffd76e9cceecfc76d2b169844732ae93ed89b
ThiagoLopes <thiagolopes621@gmail.com>
2021-03-23 22:59:05 -0300

    finish layout

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,45 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
+export function ChallengeBox(){
+    const hasActiveChallenge = true;
+
+    return(
+        <div className={styles.challengeBoxContainer}>
+            {hasActiveChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400xp</header>
+                    <main>
+                        <img src="icons/body.svg" alt=""/>
+                        <strong>Novo Desafio</strong>
+                        <p>Levante e faça uma caminhada</p>
+                    </main>
+                    
+                    <footer>
+                        <button
+                            type="button"
+                            className={styles.challengeFailedButtom}
+                        >
+                            Falhei
+                        </button>
+                        <button
+                            type="button"
+                            className={styles.challengeSucceededButtom}
+                        >
+                            Completei
+                        </button>
+
+                    </footer>
+                </div>
+            ) : (
+            <div className={styles.challengeNotActive}>
+                <strong> Finalize um ciclo para receber desafios </strong>
+                <p>
+                    <img src="icons/level-up.svg" alt="Level Up"/>
+                    Avance de level completando Desafios!
+                </p>
+            </div>
+            )}
+            
+        </div>
+    )
+}
